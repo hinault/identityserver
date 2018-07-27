65d8576b53d26611ecb697dc18e965e3c75beb12
hinault <ngdonfack@gmail.com>
2018-07-27 14:37:37 -0400

    Upadte Program.cs

--- ConsoleAppClient/Program.cs
+++ ConsoleAppClient/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace ConsoleAppClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => CallWebApi().GetAwaiter().GetResult();
+
+        static async Task CallWebApi()
+
         {
-            Console.WriteLine("Hello World!");
+            var client = new HttpClient();
+
+            var response = await client.GetAsync("https://localhost:5003/api/secure");
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+            else
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(JArray.Parse(content));
+            }
         }
     }
 }
