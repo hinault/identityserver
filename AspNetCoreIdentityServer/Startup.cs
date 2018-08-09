15860e79a7036394ebde9f96cbc3b45e89cc23ae
hinault <ngdonfack@gmail.com>
2018-08-09 14:47:10 -0400

    update startup.cs

--- AspNetCoreIdentityServer/Startup.cs
+++ AspNetCoreIdentityServer/Startup.cs
@@ -1,18 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using IdentityServer4;
+﻿
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 namespace AspNetCoreIdentityServer
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -22,9 +27,12 @@ namespace AspNetCoreIdentityServer
             //configure identity server with in-memory stores, keys, clients and resources
             services.AddIdentityServer()
                    .AddDeveloperSigningCredential()
-                   .AddInMemoryIdentityResources(Config.GetIdentityResources())
-                    .AddInMemoryApiResources(Config.GetApiResources())
-                   .AddInMemoryClients(Config.GetClients())
+                   .AddConfigurationStore(options =>
+                   {
+                       options.ConfigureDbContext = builder =>
+                           builder.UseSqlite(Configuration.GetConnectionString("DefaultConnection"),
+                               sql => sql.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name));
+                   })
                    .AddTestUsers(Config.GetUsers());
 
 
